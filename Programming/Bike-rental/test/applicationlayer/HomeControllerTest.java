12a50b1b5911172b9149d1c972681bb0cd537936
Tr??ng Quang Khánh <quangkhanh250699@gmail.com>
2020-12-15 23:17:22 +0700

    add test module

---
+++ Programming/Bike-rental/test/applicationlayer/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package applicationlayer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HomeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBriefStations() {
+    }
+}
