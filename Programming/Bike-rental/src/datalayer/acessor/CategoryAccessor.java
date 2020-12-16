d5a8dfd5d0b210a6c3deb6275a781d33477059a3
Huy nguyen <nguyenvanhuybk99@gmail.com>
2020-12-16 12:56:39 +0700

    add category accessor

---
+++ Programming/Bike-rental/src/datalayer/acessor/CategoryAccessor.java
@@ -0,0 +1,32 @@
+package datalayer.acessor;
+
+import datalayer.model.Category;
+
+import java.util.List;
+
+public class CategoryAccessor extends DataAccessor<Category>{
+    @Override
+    public Category get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(Category category) {
+
+    }
+
+    @Override
+    public void save(Category category) {
+
+    }
+
+    @Override
+    public void delete(Category category) {
+
+    }
+}
