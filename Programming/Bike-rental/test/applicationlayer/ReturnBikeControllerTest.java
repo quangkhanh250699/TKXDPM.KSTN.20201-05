e546e22196a856461af3d46a38bce88335a5348a
Tr??ng Quang Khánh <quangkhanh250699@gmail.com>
2020-12-16 10:17:22 +0700

    add category entity

---
+++ Programming/Bike-rental/test/applicationlayer/ReturnBikeControllerTest.java
@@ -0,0 +1,22 @@
+package applicationlayer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReturnBikeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void requestReturnBike() {
+    }
+}
