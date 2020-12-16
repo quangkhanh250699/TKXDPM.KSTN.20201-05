14c74906536aa38c989151fafe3ab4c6f6107ebb
Huy nguyen <nguyenvanhuybk99@gmail.com>
2020-12-16 12:49:20 +0700

    add basic logic accessor

---
+++ Programming/Bike-rental/src/datalayer/acessor/TransactionHistoryAccessor.java
@@ -0,0 +1,32 @@
+package datalayer.acessor;
+
+import datalayer.model.TransactionHistory;
+
+import java.util.List;
+
+public class TransactionHistoryAccessor extends DataAccessor<TransactionHistory>{
+    @Override
+    public TransactionHistory get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<TransactionHistory> getAll() {
+        return null;
+    }
+
+    @Override
+    public void update(TransactionHistory transactionHistory) {
+
+    }
+
+    @Override
+    public void save(TransactionHistory transactionHistory) {
+
+    }
+
+    @Override
+    public void delete(TransactionHistory transactionHistory) {
+
+    }
+}
