7afe122f0b76fadcacef36cddb950be2afc29bd8
Tr??ng Quang Khánh <quangkhanh250699@gmail.com>
2020-12-16 11:52:56 +0700

    update return bike controller on calculate cost method

--- Programming/Bike-rental/src/applicationlayer/ReturnBikeController.java
+++ Programming/Bike-rental/src/applicationlayer/ReturnBikeController.java
@@ -2,8 +2,14 @@ package applicationlayer;
 
 import datalayer.model.RentedBike;
 
+import java.sql.Timestamp;
+
 public class ReturnBikeController {
     public void requestReturnBike(RentedBike bike) {
         return;
+    }
+
+    public float calculateCost(RentedBike rentedBike, Timestamp endTime) {
+        return 0;
     }
 }
