648d37aa9d9f5caa775f9722a0f757796c49a71a
Huy nguyen <nguyenvanhuybk99@gmail.com>
2020-12-17 16:49:39 +0700

    fix query user, add rented bike

--- Programming/Bike-rental/src/datalayer/acessor/UserAccessor.java
+++ Programming/Bike-rental/src/datalayer/acessor/UserAccessor.java
@@ -1,8 +1,6 @@
 package datalayer.acessor;
 
-import datalayer.model.Category;
-import datalayer.model.Station;
-import datalayer.model.User;
+import datalayer.model.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,12 +10,14 @@ import java.util.List;
 public class UserAccessor extends DataAccessor<User>{
     @Override
     public User get(int id) {
-        String q = "SELECT * FROM station WHERE stationId = " + id;
+        String q = "SELECT * FROM user WHERE userId = " + id;
         User user = null;
         try{
             ResultSet rs = query(q);
+            BikeFactory bikeFactory = BikeFactory.getInstance();
             rs.next();
-            user = new User(id, rs.getString("name"));
+            List<Bike> bikesRent = bikeFactory.getBikeRentByUserId(id);
+            user = new User(id, rs.getString("name"), bikesRent);
 
         }catch(SQLException se){
             //Handle errors for JDBC
@@ -31,12 +31,14 @@ public class UserAccessor extends DataAccessor<User>{
 
     @Override
     public List<User> getAll() {
-        String q = "SELECT * FROM category" ;
+        String q = "SELECT * FROM user" ;
         List<User> users = new ArrayList<User>();
         try{
             ResultSet rs = query(q);
+            BikeFactory bikeFactory = BikeFactory.getInstance();
             while(rs.next()){
-                users.add(new User(rs.getInt("userId"), rs.getString("name")));
+                List<Bike> bikesRent = bikeFactory.getBikeRentByUserId(rs.getInt("userId"));
+                users.add(new User(rs.getInt("userId"), rs.getString("name"), bikesRent));
 
             }
         }catch(SQLException se){
--- Programming/Bike-rental/src/datalayer/model/User.java
+++ Programming/Bike-rental/src/datalayer/model/User.java
@@ -1,13 +1,21 @@
 package datalayer.model;
 
+import java.util.List;
+
 public class User {
 
     private int userId;
     private String name;
+    private List<Bike> rentBikes;
 
-    public User(int userId, String name) {
+    public User(int userId, String name, List<Bike> rentBikes) {
         this.userId = userId;
         this.name = name;
+        this.rentBikes = rentBikes;
+    }
+
+    public List<Bike> getRentBikes() {
+        return rentBikes;
     }
 
     public int getUserId() {
